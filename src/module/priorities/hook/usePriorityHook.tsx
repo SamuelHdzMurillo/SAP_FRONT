ae99c9c29185cfffefb0610099ad90d298b7c8a5
danieldarkmun20 <daniel_200200@live.com.mx>
2024-04-05 16:55:40 -0700

    Fix

--- src/module/priorities/hook/usePriorityHook.tsx
+++ src/module/priorities/hook/usePriorityHook.tsx
@@ -36,8 +36,8 @@ export const usePriorityHook = () => {
           (prom: PriorityChartApi, i: number) => {
             return {
               x: prom.section_name,
-              fillColor: i % 2 === 0 ? "#8f2a2b" : "#1C1C1C", // Change the color based on whether the index is even or odd
-              strokeColor: i % 2 === 0 ? "#8f2a2b" : "#1C1C1C", // Change the color based on whether the index is even or odd
+              fillColor: i % 2 === 0 ? "#8f2a2b" : "#1C1C1C",
+              strokeColor: i % 2 === 0 ? "#8f2a2b" : "#1C1C1C",
               y: prom.promoteds_count,
             };
           }
